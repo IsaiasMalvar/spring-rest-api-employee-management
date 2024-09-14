c91e5e94af3718844dfa4a551dc3bae104d0725b
Isaias Malvar <isaiasmg@93hotmail.com>
2024-09-14 19:27:35 +0100

    Add update item method in REST controller

--- src/main/java/com/restapi/springboot/crud/rest/EmployeeRestController.java
+++ src/main/java/com/restapi/springboot/crud/rest/EmployeeRestController.java
@@ -45,4 +45,12 @@ public class EmployeeRestController {
         return dbEmployee;
     }
 
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee theEmployee){
+
+        Employee dbEmployee = employeeService.save(theEmployee);
+
+        return dbEmployee;
+    }
+
 }
