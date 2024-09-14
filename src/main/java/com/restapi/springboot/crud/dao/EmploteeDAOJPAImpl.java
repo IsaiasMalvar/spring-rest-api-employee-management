6269ba9b0e9f029c3c66d649a672e09a3bec3f58
Isaias Malvar <isaiasmg@93hotmail.com>
2024-09-14 18:15:23 +0100

    Create initial Employee DAO implementation

---
+++ src/main/java/com/restapi/springboot/crud/dao/EmploteeDAOJPAImpl.java
@@ -0,0 +1,30 @@
+package com.restapi.springboot.crud.dao;
+
+import com.restapi.springboot.crud.entity.Employee;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class EmploteeDAOJPAImpl implements EmployeeDAO{
+
+    private EntityManager entityManager;
+
+    @Autowired
+    public EmploteeDAOJPAImpl(EntityManager theEntityManager) {
+        entityManager = theEntityManager;
+    }
+
+    @Override
+    public List<Employee> findAll() {
+
+        TypedQuery<Employee> theQuery = entityManager.createQuery("FROM Employee", Employee.class);
+
+        List<Employee> employees = theQuery.getResultList();
+
+        return employees;
+    }
+}
